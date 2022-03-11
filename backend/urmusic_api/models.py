272aa87a49142d0ba587b539c294087e2cd6bf93
Лонин Леонид <loninlion4@gmail.com>
2022-03-11 08:59:52 +0000

    Имя пользователя не уникально, только емаил

--- backend/urmusic_api/models.py
+++ backend/urmusic_api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Account(models.Model):
     id = models.IntegerField(primary_key=True)
-    username = models.CharField(unique=True, max_length=255, default='', )
+    username = models.CharField(unique=False, max_length=255, default='', )
     password = models.CharField(max_length=255, default='')
     email = models.CharField(max_length=255, default='', unique=True)
