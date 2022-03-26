8dc3ec764b858feee19bd064fa1affaf4515a254
Arseny Porkhunov <arseny2606yt@gmail.com>
2022-03-26 16:39:45 +0300

    implement track list on restaurant page

--- frontend/src/panels/Restaurant.js
+++ frontend/src/panels/Restaurant.js
@@ -1,9 +1,23 @@
-import {ContentCard, Group, Panel, PanelHeader, PanelHeaderBack} from "@vkontakte/vkui";
+import {
+    Card,
+    CardGrid,
+    Cell, CellButton,
+    ContentCard,
+    Footer,
+    Group,
+    Header,
+    Panel,
+    PanelHeader,
+    PanelHeaderBack,
+    Text
+} from "@vkontakte/vkui";
 import {useEffect, useState} from "react";
 import {replace, useParams} from "@itznevikat/router";
+import {Icon24Add} from "@vkontakte/icons";
 
 const Restaurant = ({id, nav, token, apiRequest}) => {
     const [restaurantData, setRestaurantData] = useState(undefined);
+    const [profile, setProfile] = useState(undefined);
     const {restaurant_id} = useParams();
 
     const getNoun = (number, one, two, five) => {
@@ -22,27 +36,67 @@ const Restaurant = ({id, nav, token, apiRequest}) => {
         return five;
     }
 
-    useEffect(() => {
-            function fetchData() {
+    function zfill(num, size) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num;
+    }
+
+
+    useEffect(async () => {
+            async function fetchData() {
                 if (!token) {
                     if (!localStorage.getItem("token")) {
                         replace("/login");
                         return;
                     }
                 }
-                apiRequest(`restaurants/get/?id=${restaurant_id}&`).then(response => {
-                    console.log(response);
+                apiRequest("account/profile").then(response => {
+                   setProfile(response);
+                });
+                apiRequest(`restaurants/get/?id=${restaurant_id}&`).then(async response => {
                     if (response.error) {
                         replace("/catalogue");
                         return;
+                    }
+                    for (let i = 0; i < response.tracks.length; ++i) {
+                        const duration = await getDuration(response.tracks[i].track_data.track_url);
+                        response.tracks[i].track_data.duration = `${zfill(parseInt(duration / 60), 2)}:${zfill(parseInt(duration) % 60, 2)}`;
                     }
                     setRestaurantData(response);
                 });
             }
 
-            fetchData();
+            await fetchData();
         }, [token]
     )
+
+    function getDuration(src) {
+        return new Promise(function (resolve) {
+            const audio = new Audio();
+            audio.addEventListener("loadedmetadata", function () {
+                resolve(audio.duration);
+            });
+            audio.src = src;
+        });
+    }
+
+    const deleteTrack = (id) => {
+        apiRequest("tracks/delete", `order_id=${id}`).then(() => {
+            apiRequest(`restaurants/get/?id=${restaurant_id}&`).then(async response => {
+                console.log(response);
+                if (response.error) {
+                    replace("/catalogue");
+                    return;
+                }
+                for (let i = 0; i < response.tracks.length; ++i) {
+                    const duration = await getDuration(response.tracks[i].track_data.track_url);
+                    response.tracks[i].track_data.duration = `${zfill(parseInt(duration / 60), 2)}:${zfill(parseInt(duration) % 60, 2)}`;
+                }
+                setRestaurantData(response);
+            });
+        })
+    }
 
     return (
         <Panel id={id} nav={nav}>
@@ -50,19 +104,38 @@ const Restaurant = ({id, nav, token, apiRequest}) => {
                 <PanelHeader left={<PanelHeaderBack onClick={() => replace("/catalogue")}/>}>Ресторан
                     «{restaurantData.data.name}»</PanelHeader>
                 <Group>
-                    <ContentCard
-                        onClick={() => {
-                        }}
-                        hasHover={false}
-                        hasActive={false}
-                        onEnter={() => {
-                        }}
-                        src={restaurantData.data.image_url}
-                        subtitle={restaurantData.data.address}
-                        header={restaurantData.data.name}
-                        text={restaurantData.data.description}
-                        maxHeight={500}
-                    />
+                    <CardGrid size="l">
+                        <ContentCard
+                            onClick={() => {
+                            }}
+                            hasHover={false}
+                            hasActive={false}
+                            onEnter={() => {
+                            }}
+                            src={restaurantData.data.image_url}
+                            subtitle={restaurantData.data.address}
+                            header={restaurantData.data.name}
+                            text={restaurantData.data.description}
+                            maxHeight={500}
+                        />
+                        <Card mode="shadow">
+                            <Group header={<Header>Очередь треков</Header>} mode={"plain"}>
+                                {restaurantData.tracks.length > 0 && <>{
+                                    restaurantData.tracks.map((track) => (
+                                        <Cell key={track.id} disabled mode={track.owner === profile.id ? "removable" : ""} onRemove={() => deleteTrack(track.id)}
+                                            // before={<Avatar mode="image" src={object.image_url} size={72}/>}
+                                              after={<Text>{track.track_data.duration}</Text>}
+                                              description={
+                                                  <Text>{track.track_data.artist}</Text>}> <Text
+                                            weight="medium"
+                                            style={{fontSize: 16}}>{track.track_data.title}</Text></Cell>
+                                    ))}
+                                    <Footer>{restaurantData.tracks.length} {getNoun(restaurantData.tracks.length, "трек", "трека", "треков")}</Footer></>}
+                                {restaurantData.tracks.length === 0 && <Footer>Ничего не найдено</Footer>}
+                                <CellButton before={<Icon24Add/>}>Добавить трек</CellButton>
+                            </Group>
+                        </Card>
+                    </CardGrid>
                 </Group>
             </>}
         </Panel>);
