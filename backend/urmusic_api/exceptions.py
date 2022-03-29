ae16beb7fa686b7b2800a9d79a0eff17087a102f
Arseny Porkhunov <arseny2606yt@gmail.com>
2022-03-29 17:02:04 +0300

    prettify code and fix empty order issue

--- backend/urmusic_api/exceptions.py
+++ backend/urmusic_api/exceptions.py
@@ -1,3 +1,9 @@
+import math
+
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy as _, ngettext
+from rest_framework import status
+from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler
 
 
@@ -17,3 +23,24 @@ def our_exception_handler(exc, context):
     if response is not None:
         response.data['status_code'] = response.status_code
     return response
+
+
+class OurThrottled(APIException):
+    status_code = status.HTTP_429_TOO_MANY_REQUESTS
+    default_detail = _('Превышен временной лимит.')
+    extra_detail_singular = _('Попробуйте через {wait} секунд.')
+    extra_detail_plural = _('Попробуйте через {wait} секунд.')
+    default_code = 'throttled'
+
+    def __init__(self, wait=None, detail=None, code=None):
+        if detail is None:
+            detail = force_str(self.default_detail)
+        if wait is not None:
+            wait = math.ceil(wait)
+            detail = ' '.join((
+                detail,
+                force_str(ngettext(self.extra_detail_singular.format(wait=wait),
+                                   self.extra_detail_plural.format(wait=wait),
+                                   wait))))
+        self.wait = wait
+        super().__init__(detail, code)
