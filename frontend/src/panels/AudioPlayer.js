5e5f5ba198a0979b26e8cb72f623a8448a4236c1
Egor <egorartemyev@mail.ru>
2022-05-22 14:27:00 +0300

    Player added

--- frontend/src/panels/AudioPlayer.js
+++ frontend/src/panels/AudioPlayer.js
@@ -1,3 +1,15 @@
+import {
+    Card,
+    Row,
+    Container,
+    Button,
+    Stack,
+    Collapse,
+    ListGroup,
+    Navbar,
+    Nav
+} from 'react-bootstrap';
+
 import {Panel} from "@vkontakte/vkui";
 import {useEffect, useState} from "react";
 import {replace, useParams} from "@itznevikat/router";
@@ -76,14 +88,54 @@ const AudioPlayer = ({id, nav, token, apiRequest}) => {
         }, [token, playing, started]
     )
     return (
+        <Stack gap={3}><>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossOrigin="anonymous"
+        />
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand>UrMusic</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#"></Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link>Название ресторана</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </>
         <Panel id={id} nav={nav}>
-            {!started && <button onClick={() => setStarted(true)}>start</button>}
-            {restaurantData && <>
-                <div>
-                    <p>Playing</p>
-                </div>
-            </>}
+            <Container className="center">
+                <Row className="justify-content-center">
+                    <Card style={{marginTop: '5rem', marginBottom: '5rem'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                Плеер ресторана
+                            </Card.Title>
+                            <Card.Subtitle>Сейчас играет:</Card.Subtitle>
+                            <Card.Text>
+                                {}
+                            </Card.Text>
+                            <div className="d-grid gap-1">
+                                {!started && <Button variant="primary" size="lg" onClick={() => setStarted(true)}>start</Button>}
+                            </div>
+                            {restaurantData && <>
+                                <div>
+                                    <p>Playing</p>
+                                </div>
+                            </>}
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
         </Panel>
+        </Stack>
     );
 };
 
